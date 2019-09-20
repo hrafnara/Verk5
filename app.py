c407e9ec5fa516833d4202ae47c630e6b71f9243
hrafnara <43746787+hrafnara@users.noreply.github.com>
2019-09-20 09:20:55 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<a href="/Kauplisti/">Kauplisti</a> <a href="/Karfa/">Karfa</a>'
+
+@app.route('Karfa')
+def Karfa()
+
+@app.route('Kauplisti')
+def Kauplisti()
+    return 
+    <input type="submit" value = "Kaupa" />
